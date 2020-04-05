cfd0505d7b1534319b94f3b301011492e39c457a
shaphan <chaphannzabonimana@gmail.com>
2020-04-05 15:55:56 +0200

    change audits folder

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   runner: "jest-runner-phpunit",
   displayName: "phpunit",
   moduleFileExtensions: ["php"],
-  testMatch: ["**/covid-19-estimator-audits/php/**/*Test.php"]
+  testMatch: ["**/audits/**/*Test.php"],
 };
