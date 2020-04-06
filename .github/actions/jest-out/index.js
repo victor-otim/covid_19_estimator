49e5d3a3a134e04cb36fbe29ffb519549c3ef838
Charles Odili <charlesodili@Charless-MacBook-Pro-2.local>
2020-04-06 19:10:50 +0100

    fix jest-out

--- .github/actions/jest-out/index.js
+++ .github/actions/jest-out/index.js
@@ -1,26 +1,26 @@
-const axios = require("axios");
+const axios = require('axios');
 const xml2json = require("xml2json");
-const core = require("@actions/core");
-const { context } = require("@actions/github");
+const core = require('@actions/core');
+const { context } = require('@actions/github');
 
 const CHALLENGES = {
-  "ch-1": "challenge-01",
-  "ch-2": "challenge-02",
-  "ch-3": "challenge-03",
-  "ch-4": "challenge-04",
-  "ch-5": "challenge-05",
+  'ch-1': 'challenge-01',
+  'ch-2': 'challenge-02',
+  'ch-3': 'challenge-03',
+  'ch-4': 'challenge-04',
+  'ch-5': 'challenge-05'
 };
 
 const getStatsFor = (lang, task) => {
   let stats = {};
 
-  if (lang === "javscript") {
+  if (lang === 'javscript' || ['ch-4', 'ch-5'].includes(task)) {
     const payload = require(`../../../audits/${task}.json`);
     stats.numTotalTests = payload.numTotalTests;
     stats.numPassedTests = payload.numPassedTests;
   }
 
-  if (lang === "python") {
+  if (lang === 'python') {
     // JSON:: report > summary > passed | num_tests
     const data = require(`../../../audits/${task}.json`);
     const payload = data.report.summary;
@@ -28,9 +28,10 @@ const getStatsFor = (lang, task) => {
     stats.numPassedTests = payload.passed;
   }
 
-  if (lang === "php") {
+  if (lang === 'php') {
     const xml = require(`../../../audits/${task}.xml`);
     const data = xml2json.toJson(xml, { object: true });
+    console.log(data);
     const payload = data.testsuites.testsuite[0];
     // XML:: testsuites > testsuite > tests | errors | failures (attrs)
     stats.numErrors = payload.errors;
@@ -43,24 +44,24 @@ const getStatsFor = (lang, task) => {
 
 const run = async () => {
   try {
-    const task = core.getInput("challenge");
-    const language = core.getInput("lang");
+    const task = core.getInput('challenge');
+    const language = core.getInput('lang');
     const challenge = CHALLENGES[task];
     const stats = getStatsFor(language, task);
 
-    const repo = context.repo.repo;
-    const owner = context.repo.owner;
+    const { repo, owner } = context.repo;
+    console.log('repo', context.repo);
 
     const report = {
       repo,
       owner,
       ...stats,
       language,
-      source: "jest",
-      type: challenge,
+      source: 'jest',
+      type: challenge
     };
 
-    const server = core.getInput("server");
+    const server = core.getInput('server');
     await axios.post(`${server}/entry-tests`, { report });
   } catch (error) {
     core.setFailed(error.message);
