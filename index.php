6ffb2b11e5e8056a94bf8146e517627feb3b2e74
Victor Otim <sirotim@gmail.com>
2020-04-11 21:46:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name=description content="COVID-19 Estimator, Andela-facebook challenge"/>
 <title>COVID-19 Impact Estimator</title>
 <style>
 	div.form_content {
